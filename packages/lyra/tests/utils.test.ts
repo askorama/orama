741b9821bd865b780c01cde7dee04da566120d69
Mateo Nunez <mateonunez95@gmail.com>
2022-07-13 09:16:57 +0200

    Utils test (#9)
    * test(utils): format bytes and format nanoseconds
    * chore(Utils): remove unused method

---
+++ packages/lyra/tests/utils.test.ts
@@ -0,0 +1,22 @@
+import { formatBytes, formatNanoseconds } from "../src/utils";
+
+describe("utils", () => {
+  it("should correctly format bytes", async () => {
+    expect(formatBytes(0)).toBe("0 Bytes");
+    expect(formatBytes(1)).toBe("1 Bytes");
+    expect(formatBytes(1024)).toBe("1 KB");
+    expect(formatBytes(1024 ** 2)).toBe("1 MB");
+    expect(formatBytes(1024 ** 3)).toBe("1 GB");
+    expect(formatBytes(1024 ** 4)).toBe("1 TB");
+    expect(formatBytes(1024 ** 5)).toBe("1 PB");
+    expect(formatBytes(1024 ** 6)).toBe("1 EB");
+    expect(formatBytes(1024 ** 7)).toBe("1 ZB");
+  });
+
+  it("should correctly format nanoseconds", async () => {
+    expect(formatNanoseconds(1_000_000n)).toBe("1ms");
+    expect(formatNanoseconds(1_000_000_000n)).toBe("1000ms"); // 1s
+    expect(formatNanoseconds(1_000_000_000_000n)).toBe("1000000ms"); // 1m
+    expect(formatNanoseconds(1_000_000_000_000_000n)).toBe("1000000000ms"); // 1h
+  });
+});
