27bfb3fe6b8b9e9eaa2244c0a03d0f477fb9654f
Michele Riva <ciao@micheleriva.it>
2022-07-19 09:21:22 +0200

    feat(lyra): adds possibility to disable stemming globally
    impl #28

--- packages/lyra/src/tokenizer.ts
+++ packages/lyra/src/tokenizer.ts
@@ -5,8 +5,14 @@ const tokenizer = new WordTokenizer();
 
 export function tokenize(
   input: string,
-  language: Language = "english"
+  language: Language = "english",
+  enableStemming = true
 ): Set<string> {
   const tokens = tokenizer.tokenize(input);
+
+  if (!enableStemming) {
+    return new Set(tokens);
+  }
+
   return new Set(stemArray(tokens, language));
 }
