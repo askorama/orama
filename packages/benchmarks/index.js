04923eee258550154da6fec90dbccbd58b5693ae
Daniele Fedeli <danieligno10@gmail.com>
2022-07-22 20:34:16 +0200

    chore(query clauses): decomment unrelated benchmark

--- packages/benchmarks/index.js
+++ packages/benchmarks/index.js
@@ -1,14 +1,14 @@
 import cronometro from "cronometro";
-// import prefixSearch from "./engines/prefix-search.js";
-// import exactSearch from "./engines/exact-search.js";
-// import indexing from "./engines/indexing.js";
-// import typoTolerance from "./engines/typo-tolerance.js";
-// import prefixSearchMovies from "./engines/prefix-search-movies.js";
+import prefixSearch from "./engines/prefix-search.js";
+import exactSearch from "./engines/exact-search.js";
+import indexing from "./engines/indexing.js";
+import typoTolerance from "./engines/typo-tolerance.js";
+import prefixSearchMovies from "./engines/prefix-search-movies.js";
 import queryClauses from "./engines/query-clauses.js";
 
-// cronometro(indexing);
-// cronometro(prefixSearch);
-// cronometro(exactSearch);
-// cronometro(typoTolerance);
-// cronometro(prefixSearchMovies);
+cronometro(indexing);
+cronometro(prefixSearch);
+cronometro(exactSearch);
+cronometro(typoTolerance);
+cronometro(prefixSearchMovies);
 cronometro(queryClauses);
