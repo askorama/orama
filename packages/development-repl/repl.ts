257e759fc08a2de0233c054766d5767320187268
Michele Riva <ciao@micheleriva.it>
2022-08-01 11:05:08 +0200

    chore(development-repl): adds basic development repl

---
+++ packages/development-repl/repl.ts
@@ -0,0 +1,106 @@
+import * as fs from "node:fs";
+import * as readline from "node:readline";
+import * as util from "node:util";
+import { create, insert, search, SearchParams } from "@nearform/lyra";
+import yargs from "yargs";
+import progress from "cli-progress";
+
+const db = create({
+  schema: {
+    type: "string",
+    title: "string",
+    category: "string",
+  },
+});
+
+async function populateDB() {
+  let elementCounts = 0;
+  const bar = new progress.SingleBar({}, progress.Presets.shades_classic);
+
+  const fileStream = fs.createReadStream("./dataset/title.tsv");
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  bar.start(1_000_000, 0);
+
+  for await (const row of rl) {
+    const [, type, title, , , , , , category] = row.split("\t");
+
+    insert(db, {
+      type,
+      title,
+      category,
+    });
+    bar.update(elementCounts++);
+  }
+
+  bar.stop();
+}
+
+function log(cmd: string) {
+  console.log(util.inspect(cmd, true, null, true));
+}
+
+function parseCommand(cmd: string) {
+  const y = yargs(cmd as unknown as string[]);
+
+  return y
+    .option("populate", {
+      alias: "p",
+      describe: "Populate the database",
+    })
+    .option("search", {
+      alias: "s",
+      describe: "Search the database",
+    })
+    .option("limit", {
+      alias: "l",
+      describe: "Limit the number of results",
+    })
+    .option("offset", {
+      alias: "o",
+      describe: "Offset the results",
+    })
+    .option("exact", {
+      alias: "e",
+      describe: "Search for an exact match",
+    })
+    .option("tolerance", {
+      alias: "t",
+      describe: "Tolerance for fuzzy search",
+    }).argv;
+}
+
+function performSearch(cmd: SearchParams<any>) {
+  return search(db, {
+    term: cmd.term,
+    limit: cmd.limit,
+    offset: cmd.offset,
+    exact: cmd.exact,
+    tolerance: cmd.tolerance,
+  });
+}
+
+console.clear();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+  prompt: "> ",
+});
+
+rl.on("line", async (cmd: string) => {
+  const parsed = await parseCommand(cmd);
+
+  if (parsed.populate) {
+    await populateDB();
+    return;
+  }
+
+  const searchResult = performSearch(parsed as unknown as SearchParams<any>) as any;
+  log(searchResult);
+});
