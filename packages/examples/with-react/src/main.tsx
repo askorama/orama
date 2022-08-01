ec240c601e50ff7fa5614b7e86042c7e73de5714
Mateo Nunez <mateonunez95@gmail.com>
2022-08-01 14:54:32 +0200

    Improve React Example (#72)
    * chore(packages): add build lyra script
    * feat(example): add pokemon entity type
    * perf(*): improve style and add elapsed time

--- packages/examples/with-react/src/main.tsx
+++ packages/examples/with-react/src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'uno.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "uno.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className='flex w-full justify-center'>
+    <div className="flex w-full justify-center font-sans">
       <App />
     </div>
   </React.StrictMode>
-)
+);
