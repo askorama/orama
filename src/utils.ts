6cd1179fe28283a36f81b8aefbdac20d0890a978
jkomyno <skiabo97@gmail.com>
2022-11-22 01:36:09 +0100

    feat(lyra): replaced "intersectTokenScores" with its Wasm counterpart

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import * as wasm from "./wasm/lyra_utils_wasm";
 import type { TokenScore } from "./lyra";
 
 const baseId = Date.now().toString().slice(5);
@@ -73,46 +74,8 @@ export function getTokenFrequency(token: string, tokens: string[]): number {
 // MIT Licensed (https://github.com/lovasoa/fast_array_intersect/blob/master/LICENSE)
 // while on tag https://github.com/lovasoa/fast_array_intersect/tree/v1.1.0
 export function intersectTokenScores(arrays: TokenScore[][]): TokenScore[] {
-  if (arrays.length === 0) return [];
-
-  for (let i = 1; i < arrays.length; i++) {
-    if (arrays[i].length < arrays[0].length) {
-      const tmp = arrays[0];
-      arrays[0] = arrays[i];
-      arrays[i] = tmp;
-    }
-  }
-
-  const set: Map<string, [number, number]> = new Map();
-  for (const elem of arrays[0]) {
-    set.set(elem[0], [1, elem[1]]);
-  }
-
-  const arrLength = arrays.length;
-  for (let i = 1; i < arrLength; i++) {
-    let found = 0;
-    for (const elem of arrays[i]) {
-      const [count, score] = set.get(elem[0] ?? "") ?? [0, 0];
-      if (count === i) {
-        set.set(elem[0] ?? "", [count + 1, score + elem[1]]);
-        found++;
-      }
-    }
-
-    if (found === 0) {
-      return [];
-    }
-  }
-
-  const result: TokenScore[] = [];
-
-  for (const [token, [count, score]] of set) {
-    if (count === arrLength) {
-      result.push([token, score]);
-    }
-  }
-
-  return result;
+  const { data } = wasm.intersectTokenScores({ data: arrays });
+  return data;
 }
 
 export function insertSortedValue(
