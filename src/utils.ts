03f943b53927e4c41845baa6dbaefc5ec517c100
mateonunez <mateonunez95@gmail.com>
2023-01-03 19:08:50 +0100

    perf(utils): prevent Object.hasOwn from not being defined

--- src/utils.ts
+++ src/utils.ts
@@ -60,6 +60,11 @@ export function uniqueId(): string {
 }
 
 export function getOwnProperty<T = unknown>(object: Record<string, T>, property: string): T | undefined {
+  // Checks if `hasOwn` method is defined avoiding errors with older Node.js versions
+  if (Object.hasOwn === undefined) {
+    return Object.prototype.hasOwnProperty.call(object, property) ? object[property] : undefined;
+  }
+
   return Object.hasOwn(object, property) ? object[property] : undefined;
 }
 
