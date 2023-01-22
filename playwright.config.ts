3302cc3f0c0aa28ee88cd93c2bdfd7bd229922d0
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-01-22 18:55:16 +0100

    feat: playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,10 @@ import { devices } from '@playwright/test';
  */
 const config: PlaywrightTestConfig = {
   testDir: './tests/ci/playwright',
+  webServer: {
+    command: 'npx vite --config ./tests/ci/playwright/vite.config.js',
+    port: 3000
+  },
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -36,8 +40,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
+    baseURL: 'http://localhost:3000/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
--- tests/ci/playwright/browsers.spec.ts
+++ tests/ci/playwright/browsers.spec.ts
@@ -1,7 +1,23 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test("works correctly", async ({ page }) => {
+  await page.goto("/");
 
-  await expect(page).toHaveTitle(/Playwright/);
+  const parsedLyraResult = await page.evaluate(() => JSON.parse(document.getElementById("searchResult")!.innerHTML));
+
+  expect(parsedLyraResult).toMatchObject({
+    hits: [
+      {
+        score: 0,
+        document: {
+          title: "The prestige",
+          director: "Christopher Nolan",
+          plot: "Two friends and fellow magicians become bitter enemies after a sudden tragedy. As they devote themselves to this rivalry, they make sacrifices that bring them fame but with terrible consequences.",
+          year: 2006,
+          isFavorite: true,
+        },
+      },
+    ],
+    count: 1,
+  });
 });
