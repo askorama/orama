6ee3bc4ef0fd31da0ee622225ffbda3d63da6818
Paolo Insogna <paolo@cowtech.it>
2023-01-24 11:57:21 +0000

    test: Fixed browser tests.

--- tests/ci/playwright/browsers.spec.ts
+++ tests/ci/playwright/browsers.spec.ts
@@ -3,17 +3,12 @@ import { expect, test } from "@playwright/test";
 test("works correctly", async ({ page }) => {
   await page.goto("/");
 
-  const parsedLyraResult = await page.evaluate(() => JSON.parse(document.getElementById("searchResult")!.innerHTML));
-
-  const result = parsedLyraResult.hits.map((hit) => {
-    delete hit.score;
-    return hit;
-  });
+  const result = await page.evaluate(() => JSON.parse(document.getElementById("searchResult")!.innerHTML));
 
   expect(result).toMatchObject({
     hits: [
       {
-        score: 0.40867885542155263,
+        score: expect.any(Number),
         document: {
           title: "The prestige",
           director: "Christopher Nolan",
