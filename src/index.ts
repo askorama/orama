d5cc4ce889c55c575446f6636e23b726e34565f1
Paolo Insogna <paolo@cowtech.it>
2023-01-31 10:52:13 +0100

    fix: Added missing imports. (#268)

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,9 @@ export { load } from "./methods/load.js";
 export { remove } from "./methods/remove.js";
 export { save } from "./methods/save.js";
 export { search } from "./methods/search.js";
+
 export * from "./types.js";
+export type { Language } from "./tokenizer/languages.js";
+export type { InsertConfig, InsertBatchConfig } from "./methods/insert.js";
+export type { RetrievedDoc, SearchParams, SearchResult } from "./methods/search.js";
+export type { Stemmer, TokenizerConfig, Tokenizer } from "./tokenizer/index.js";
--- src/internals.ts
+++ src/internals.ts
@@ -1,3 +1,3 @@
 export { boundedLevenshtein } from "./levenshtein.js";
 export { formatNanoseconds, getNanosecondsTime } from "./utils.js";
-export { tokenize } from "./tokenizer/index.js";
+export { tokenize, normalizationCache, defaultTokenizerConfig } from "./tokenizer/index.js";
