e2f6513aa04fc86c2ce4d9c8aa0dea233f6681be
Michele Riva <ciao@micheleriva.it>
2023-02-10 13:24:16 +0100

    feat(filters): improves types, co-authored by @Ceres6

--- src/types/filters.ts
+++ src/types/filters.ts
@@ -1,12 +1,15 @@
 import type { PropertiesSchema } from "src/types/index.js";
 
-type ComparisonOperator =
-  | { gt?: number; } 
-  | { gte?: number; } 
-  | { lt?: number; } 
-  | { lte?: number; } 
-  | { eq?: number; } 
-  | { between?: [number, number]; };
+type ComparisonOperator = {
+  gt?: number;
+  gte?: number;
+  lt?: number;
+  lte?: number;
+  eq?: number;
+  between?: [number, number];
+}
+
+type PickOne<T> = { [P in keyof T]: Record<P, T[P]> & Partial<Record<Exclude<keyof T, P>, undefined>> }[keyof T]
 
 export type WhereFilter<
   S extends PropertiesSchema,
@@ -15,6 +18,6 @@ export type WhereFilter<
   ? S[K] extends PropertiesSchema
     ? WhereFilter<S[K], `${P}${K}.`>
     : S[K] extends "number"
-      ? { [key in `${P}${K}`]?: ComparisonOperator }
+      ? { [key in `${P}${K}`]?: PickOne<ComparisonOperator> }
       : never
   : never;
