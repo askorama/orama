39cfacffff12424785cdedebdbd0667a9b36b788
Michele Riva <ciao@micheleriva.it>
2023-02-14 10:45:11 +0100

    fix: improves types

--- src/types/filters.ts
+++ src/types/filters.ts
@@ -8,22 +8,24 @@ export type FilterOperation =
   | "eq"
   | "between";
 
-type ComparisonOperator =
-  | { gt?: number; } 
-  | { gte?: number; } 
-  | { lt?: number; } 
-  | { lte?: number; } 
-  | { eq?: number; } 
-  | { between?: [number, number]; };
+type ComparisonOperator = {
+  gt?: number;
+  gte?: number;
+  lt?: number;
+  lte?: number;
+  eq?: number;
+  between?: [number, number];
+}
+
+type PickOne<T> = { [P in keyof T]: Record<P, T[P]> & Partial<Record<Exclude<keyof T, P>, undefined>> }[keyof T]
 
 export type WhereFilter<
   S extends PropertiesSchema,
   P extends string = "",
-  K extends keyof S = keyof S
-> = K extends string
+  K extends keyof S = keyof S> = K extends string
   ? S[K] extends PropertiesSchema
     ? WhereFilter<S[K], `${P}${K}.`>
     : S[K] extends "number"
-      ? { [key in `${P}${K}`]?: ComparisonOperator }
+      ? { [key in `${P}${K}`]?: PickOne<ComparisonOperator> }
       : never
-    : never;
+  : never;
