eda7b9a2609a71397ab02f56862621b4ea9759e5
mateonunez <mateonunez95@gmail.com>
2023-03-04 12:13:07 +0100

    chore(errors): remove unused errors
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- src/errors.ts
+++ src/errors.ts
@@ -4,10 +4,6 @@ function formatJSON(input: object) {
   return JSON.stringify(input, null, 2);
 }
 
-export function INVALID_SCHEMA_TYPE(type: string): string {
-  return `Invalid schema type. Expected string or object, but got ${type}`;
-}
-
 export function INVALID_DOC_SCHEMA(expected: object, found: object): string {
   return `Invalid document structure. \nLyra has been initialized with the following schema: \n\n${formatJSON(
     expected,
@@ -18,18 +14,6 @@ export function INVALID_PROPERTY(name: string, expected: string[]): string {
   return `Invalid property name. Expected a wildcard string ("*") or array containing one of the following properties: ${expected.join(
     ", ",
   )}, but got: ${name}`;
-}
-
-export function CANT_DELETE_DOC_NOT_FOUND(id: string): string {
-  return `Document with ID ${id} does not exist.`;
-}
-
-export function CANT_DELETE_DOCUMENT(docID: string, key: string, token: string): string {
-  return `Unable to delete document "${docID}" from index "${key}" on word "${token}".`;
-}
-
-export function UNSUPPORTED_NESTED_PROPERTIES(): string {
-  return `Nested properties are not supported in this Lyra version, but will be in the future.`;
 }
 
 export function DOC_ID_DOES_NOT_EXISTS(id: string): string {
