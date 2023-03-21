256fd1b528a5cd10a107e678b5bb32e972a978b4
Michele Riva <ciao@micheleriva.it>
2023-03-21 10:02:55 +0100

    refactor(lint): adds linting

--- packages/plugin-docusaurus-sandbox/babel.config.js
+++ packages/plugin-docusaurus-sandbox/babel.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset'), ['@babel/preset-react', { runtime: 'classic' }]]
+  presets: [require.resolve('@docusaurus/core/lib/babel/preset'), ['@babel/preset-react', { runtime: 'classic' }]],
 }
