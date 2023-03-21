9e5d99edb39a7ea156269bf5b8fed734bba12356
Michele Riva <ciao@micheleriva.it>
2023-03-21 16:55:48 +0100

    docs: improves docs

--- packages/docs/theme.config.jsx
+++ packages/docs/theme.config.jsx
@@ -1,13 +1,38 @@
+import { FaSlack } from 'react-icons/fa'
+import { Logo } from './components/Logo'
+
 export default {
   project: {
     link: 'https://github.com/oramasearch/orama',
   },
-  logo: <strong>Orama</strong>,
-  footer: <p>Apache 2.0 - 2023 © OramaSearch Inc.</p>,
+  logo: <Logo />,
+  chat: {
+    link: 'https://join.slack.com/t/lyrasearch/shared_invite/zt-1gzvj0mmt-yJhJ6pnrSGuwqPmPx9uO5Q',
+    icon: <FaSlack style={{ width: 24, height: 24 }} />,
+  },
+  footer: {
+    text: <p>Apache 2.0 - 2023 © OramaSearch Inc.</p>,
+  },
   docsRepositoryBase: 'https://github.com/oramasearch/orama/blob/packages/docs/pages',
+  darkMode: false,
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Orama',
     }
-  }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Orama" />
+      <meta property="og:description" content="Search, everywhere." />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#000000"></meta>
+    </>
+  ),
 }
