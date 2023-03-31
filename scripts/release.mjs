0d953e07dfa7bd223c87fe3770ee42bfcf5d9def
Paolo Insogna <paolo@cowtech.it>
2023-03-31 12:08:35 +0200

    chore: Added releasing script.

---
+++ scripts/release.mjs
@@ -0,0 +1,60 @@
+import { spawn } from 'node:child_process'
+import { cp } from 'node:fs/promises'
+import { resolve, relative } from 'node:path'
+
+const rootDir = process.cwd()
+const packages = [
+  'orama',
+  'plugin-astro',
+  'plugin-data-persistence',
+  'plugin-docusaurus',
+  'plugin-match-highlight',
+  'plugin-parsedoc',
+]
+
+function step(message) {
+  console.log(`\x1b[1m\x1b[32m--- ${message}\x1b[0m`)
+}
+
+async function execute(command, args, cwd) {
+  if (!Array.isArray(args)) {
+    args = [args]
+  }
+
+  let success, fail
+  const promise = new Promise((resolve, reject) => {
+    success = resolve
+    fail = reject
+  })
+
+  if (cwd) {
+    step(`Executing: ${command} ${args.join(' ')} (from folder ${relative(rootDir, cwd)}) ...`)
+  } else {
+    step(`Executing: ${command} ${args.join(' ')}  ...`)
+  }
+
+  const childProcess = spawn(command, args, { cwd, stdio: 'inherit' })
+
+  childProcess.on('close', code => {
+    if (code !== 0) {
+      fail(new Error(`Process failed with status code ${code}.`))
+    }
+
+    success()
+  })
+
+  return promise
+}
+
+async function main() {
+  await execute('turbo', 'build')
+  await execute('turbo', 'test')
+  await cp('README.md', 'packages/orama/README.md')
+
+  for (const pkg of packages) {
+    const cwd = resolve(rootDir, 'packages', pkg)
+    await execute('pnpm', ['publish'], cwd)
+  }
+}
+
+await main()
--- scripts/version.mjs
+++ scripts/version.mjs
@@ -15,51 +15,55 @@ const packages = [
   'plugin-parsedoc',
 ].filter(p => !skip.includes(p))
 
-if (!process.argv[2]) {
-  console.error('\x1b[33mUsage: node version.mjs [VERSION | CHANGE [PRERELEASE]]\x1b[0m')
-  process.exit(1)
-}
-
-// Check if the new version is absolute, otherwise we take it from @orama/orama and increase
-let version = process.argv[2]
-
-if (!valid(version)) {
-  version = inc(oramaPackage.version, version, process.argv[3])
-}
-
-if (!valid(version)) {
-  console.error(
-    `\x1b[31mCannot increase version \x1b[1m${oramaPackage.version}\x1b[22m using operator \x1b[1m${process.argv[2]}\x1b[22m.\x1b[0m`,
-  )
-  process.exit(1)
-}
-
-// Perform the update
-for (const pkg of packages) {
-  const packageJsonPath = fileURLToPath(new URL(`../packages/${pkg}/package.json`, import.meta.url))
-  const packageJson = JSON.parse(await readFile(packageJsonPath))
-
-  console.log(
-    `\x1b[32mUpdating package \x1b[1m${packageJson.name}\x1b[22m from \x1b[1m${packageJson.version}\x1b[22m to version \x1b[1m${version}\x1b[22m ...\x1b[0m`,
-  )
-  packageJson.version = version
-
-  if (process.env.DRY_RUN) {
-    continue
+async function main() {
+  if (!process.argv[2]) {
+    console.error('\x1b[33mUsage: node version.mjs [VERSION | CHANGE [PRERELEASE]]\x1b[0m')
+    process.exit(1)
   }
 
-  await writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8')
+  // Check if the new version is absolute, otherwise we take it from @orama/orama and increase
+  let version = process.argv[2]
+
+  if (!valid(version)) {
+    version = inc(oramaPackage.version, version, process.argv[3])
+  }
+
+  if (!valid(version)) {
+    console.error(
+      `\x1b[31mCannot increase version \x1b[1m${oramaPackage.version}\x1b[22m using operator \x1b[1m${process.argv[2]}\x1b[22m.\x1b[0m`,
+    )
+    process.exit(1)
+  }
+
+  // Perform the update
+  for (const pkg of packages) {
+    const packageJsonPath = fileURLToPath(new URL(`../packages/${pkg}/package.json`, import.meta.url))
+    const packageJson = JSON.parse(await readFile(packageJsonPath))
+
+    console.log(
+      `\x1b[32mUpdating package \x1b[1m${packageJson.name}\x1b[22m from \x1b[1m${packageJson.version}\x1b[22m to version \x1b[1m${version}\x1b[22m ...\x1b[0m`,
+    )
+    packageJson.version = version
+
+    if (process.env.DRY_RUN) {
+      continue
+    }
+
+    await writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8')
+  }
+
+  // Make the commit, including the tag
+  if (!process.env.DRY_RUN) {
+    const git = simpleGit({ baseDir: fileURLToPath(new URL('..', import.meta.url)) })
+    console.log(`\x1b[32mCommitting changes and creating a tag (pushing is left to you) ...\x1b[0m`)
+    await git.commit(
+      'chore: Version bump',
+      packages.map(p => `packages/${p}/package.json`),
+      { '--no-verify': true },
+    )
+
+    await git.addTag(`v${version}`)
+  }
 }
 
-// Make the commit, including the tag
-if (!process.env.DRY_RUN) {
-  const git = simpleGit({ baseDir: fileURLToPath(new URL('..', import.meta.url)) })
-  console.log(`\x1b[32mCommitting changes and creating a tag (pushing is left to you) ...\x1b[0m`)
-  await git.commit(
-    'chore: Version bump',
-    packages.map(p => `packages/${p}/package.json`),
-    { '--no-verify': true },
-  )
-
-  await git.addTag(`v${version}`)
-}
+await main()
