f1202e7c1c8d36d08980d05dfcc9cfa18c5db38a
Marco Ippolito <marcoippolito54@gmail.com>
2023-06-02 16:49:56 +0200

    fix: strict check for reference

--- packages/orama/src/trees/avl.ts
+++ packages/orama/src/trees/avl.ts
@@ -258,7 +258,7 @@ export function insert<K, V>(root: Node<K, V>, key: K, value: V): Node<K, V> {
         parent = rotateLeft(parent);
     }
 
-    if (parent == root) {
+    if (parent === root) {
         break;
     }
 
