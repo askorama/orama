02e2bd1ea6ef16c0b2dae429bc9e8debe8ac106f
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-07-21 22:13:16 +0200

    feat: first implementation

---
+++ packages/schema/src/index.ts
@@ -0,0 +1,3 @@
+import { schemaFromJson } from './jsonSchema'
+
+export { schemaFromJson }
---
+++ packages/schema/src/jsonSchema.ts
@@ -0,0 +1,45 @@
+import type { JSONSchema4 } from 'json-schema';
+import type { Schema } from '@orama/orama';
+
+const assertTypeObject = (jsonSchema: JSONSchema4) => {
+    if (jsonSchema.type !== 'object') {
+        throw new Error('Provided schema must be an object type');
+    }
+}
+
+const assertProperties = (jsonSchema: JSONSchema4) => {
+    if (!jsonSchema.properties) {
+        throw new Error('Provided must have properties');
+    }
+}
+
+const ORAMA_SUPPORTED_TYPES: Set<JSONSchema4['type']> = new Set(['string', 'number', 'boolean'])
+
+const isArraySupportedByOrama = (jsonSchema: JSONSchema4) => {
+    if (jsonSchema.type === 'array' && jsonSchema.items && !Array.isArray(jsonSchema.items)) {
+        return ORAMA_SUPPORTED_TYPES.has(jsonSchema.items.type);
+    }
+    return false
+}
+
+const isSupportedByOrama = (jsonSchema: JSONSchema4) => {
+    return ORAMA_SUPPORTED_TYPES.has(jsonSchema.type) || isArraySupportedByOrama(jsonSchema);
+}
+
+const extractOramaType = (jsonSchema: JSONSchema4) => {
+    return ORAMA_SUPPORTED_TYPES.has(jsonSchema.type) ? jsonSchema.type : `${(jsonSchema.items as JSONSchema4)!.type}[]`
+}
+
+export const schemaFromJson = (jsonSchema: JSONSchema4) => {
+    assertTypeObject(jsonSchema)
+
+    assertProperties(jsonSchema)
+
+    const oramaSchema: Schema = {}
+
+    for (const [propertyName, propertyDefinition] of Object.entries(jsonSchema.properties!)) {
+        if (isSupportedByOrama(propertyDefinition)) {
+            oramaSchema[propertyName] = extractOramaType(propertyDefinition)
+        }
+    }
+}
