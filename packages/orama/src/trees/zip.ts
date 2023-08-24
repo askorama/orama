3bc9d59faa237bc76074169cd15a242e452acdb1
Michele Riva <ciao@micheleriva.it>
2023-08-24 11:13:26 -0700

    orama(dsa): fixes zip tree and adds tests

--- packages/orama/src/trees/zip.ts
+++ packages/orama/src/trees/zip.ts
@@ -1,68 +1,102 @@
 import { Nullable } from '../types.js'
 
-type Node<V> = {
+interface Node<V = unknown> {
   left: Nullable<Node<V>>
   right: Nullable<Node<V>>
+  parent: Nullable<Node<V>>
   key: number
   value: V
   rank: number
 }
 
-export function create<V>(key: number, value: V): Node<V> {
+function randomRank (maxAttempts: number = 32): number {
+  let heads = 0
+
+  for (let i = 0; i < maxAttempts; i++) {
+    if (Math.random() >= 0.5) break
+    heads += 1
+  }
+
+  return heads
+}
+
+export function create<V> (key: number, value: V): Node<V> {
   return {
     value,
     key,
-    rank: Math.random(),
+    rank: randomRank(),
     left: null,
-    right: null
+    right: null,
+    parent: null
   }
 }
 
-export function insert<V>(root: Nullable<Node<V>>, key: number, value: V): Nullable<Node<V>> {
+export function insert<V> (root: Node<V>, key: number, value: V): void {
   const newNode = create(key, value)
-
-  if (root === null) {
-    return newNode
+  if (!root) {
+    return
   }
 
-  const [left, right] = split(root, key)
+  let currentNode: Nullable<Node<V>> = root
+  let parent: Nullable<Node<V>> = null
 
-  return merge(merge(left, newNode), right)
+  while (currentNode != null) {
+    parent = currentNode
+    if (key < currentNode.key) {
+      currentNode = currentNode.left
+    } else if (key > currentNode.key) {
+      currentNode = currentNode.right
+    } else {
+      currentNode.value = value
+      return
+    }
+  }
+
+  if ((parent != null) && key < parent.key) {
+    parent.left = newNode
+  } else {
+    if (parent != null) {
+      parent.right = newNode
+    }
+  }
+  newNode.parent = parent
 }
 
-export function remove<V>(root: Nullable<Node<V>>, key: number): Nullable<Node<V>> {
+export function remove<V> (root: Nullable<Node<V>>, key: number): Nullable<Node<V>> {
   if (root === null) {
     return null
   }
 
-  const [left, right] = split(root, key)
-  const [, value] = split(right, key + 1)
+  const [left, mid] = split(root, key)
+  const [toDelete, right] = split(mid, key + 1)
+  const newRoot = merge(left, right)
 
-  return merge(left, value)
+  return newRoot
 }
 
-export function find<V>(root: Nullable<Node<V>>, key: number): Nullable<Node<V>> {
+export function find<V> (root: Nullable<Node<V>>, key: number): Nullable<V> {
   let currentNode: Nullable<Node<V>> = root
+
   while (currentNode !== null) {
-    if (currentNode.value === key) {
-      return currentNode
+    if (currentNode.key === key) {
+      return currentNode.value
     }
     currentNode = (currentNode.key < key) ? currentNode.right : currentNode.left
   }
+
   return null
 }
 
-export function contains<V>(root: Nullable<Node<V>>, key: number): boolean {
+export function contains<V> (root: Nullable<Node<V>>, key: number): boolean {
   return !!find(root, key)
 }
 
-export function rangeSearch<V>(root: Nullable<Node<V>>, min: number, max: number): Node<V>[] {
-  const results: Node<V>[] = []
-  const stack: Nullable<Node<V>>[] = []
-  let currentNode = root;
+export function rangeSearch<V> (root: Nullable<Node<V>>, min: number, max: number): V[] {
+  const results: V[] = []
+  const stack: Array<Nullable<Node<V>>> = []
+  let currentNode = root
 
   while (currentNode !== null || stack.length > 0) {
-
     while (currentNode !== null) {
       stack.push(currentNode)
       currentNode = currentNode.left
@@ -70,8 +104,7 @@ export function rangeSearch<V>(root: Nullable<Node<V>>, min: number, max: number
 
     currentNode = stack.pop()!
     if (currentNode.key >= min && currentNode.key <= max) {
-
-      results.push(currentNode)
+      results.push(currentNode.value)
     }
 
     currentNode = currentNode.right
@@ -80,9 +113,9 @@ export function rangeSearch<V>(root: Nullable<Node<V>>, min: number, max: number
   return results
 }
 
-export function greaterThan<V>(root: Nullable<Node<V>>, key: number): Node<V>[] {
-  const results: Node<V>[] = []
-  const stack: Nullable<Node<V>>[] = []
+export function greaterThan<V> (root: Nullable<Node<V>>, key: number): V[] {
+  const results: V[] = []
+  const stack: Array<Nullable<Node<V>>> = []
   let currentNode = root
 
   while (currentNode !== null || stack.length > 0) {
@@ -93,7 +126,7 @@ export function greaterThan<V>(root: Nullable<Node<V>>, key: number): Node<V>[] 
 
     currentNode = stack.pop()!
     if (currentNode.key > key) {
-      results.push(currentNode)
+      results.push(currentNode.value)
     }
 
     currentNode = currentNode.right
@@ -102,9 +135,9 @@ export function greaterThan<V>(root: Nullable<Node<V>>, key: number): Node<V>[] 
   return results
 }
 
-export function lessThan<V>(root: Nullable<Node<V>>, key: number): Node<V>[] {
-  const results: Node<V>[] = []
-  const stack: Nullable<Node<V>>[] = []
+export function lessThan<V> (root: Nullable<Node<V>>, key: number): V[] {
+  const results: V[] = []
+  const stack: Array<Nullable<Node<V>>> = []
   let currentNode = root
 
   while (currentNode !== null || stack.length > 0) {
@@ -115,7 +148,7 @@ export function lessThan<V>(root: Nullable<Node<V>>, key: number): Node<V>[] {
 
     currentNode = stack.pop()!
     if (currentNode.key < key) {
-      results.push(currentNode)
+      results.push(currentNode.value)
     }
 
     currentNode = currentNode.right
@@ -124,8 +157,8 @@ export function lessThan<V>(root: Nullable<Node<V>>, key: number): Node<V>[] {
   return results
 }
 
-export function getSize<V>(root: Nullable<Node<V>>): number {
-  const stack: Nullable<Node<V>>[] = []
+export function getSize<V> (root: Nullable<Node<V>>): number {
+  const stack: Array<Nullable<Node<V>>> = []
   let count = 0
   let currentNode = root
 
@@ -143,88 +176,50 @@ export function getSize<V>(root: Nullable<Node<V>>): number {
   return count
 }
 
-
-function split<V>(root: Nullable<Node<V>>, key: number): [Nullable<Node<V>>, Nullable<Node<V>>] {
+function split<V> (root: Nullable<Node<V>>, key: number): [Nullable<Node<V>>, Nullable<Node<V>>] {
   let left: Nullable<Node<V>> = null
+  let leftTail: Nullable<Node<V>> = null
   let right: Nullable<Node<V>> = null
+  let rightTail: Nullable<Node<V>> = null
+  let currentNode = root
 
-  let currentNode: Nullable<Node<V>> = root
   while (currentNode !== null) {
     if (currentNode.key < key) {
-      if (currentNode.right === null) {
-        right = null
-        left = currentNode
-        break
-      } else if (currentNode.right.rank < currentNode.rank) {
-        const tmp = currentNode.right
-        currentNode.right = tmp.left
-        tmp.left = currentNode
-        currentNode = tmp
+      if (leftTail != null) {
+        leftTail.right = currentNode
+        currentNode.parent = leftTail
       } else {
         left = currentNode
-        currentNode = currentNode.right
       }
+      leftTail = currentNode
+      currentNode = currentNode.right
+      leftTail.right = null
     } else {
-      if (currentNode.left === null) {
-        left = null
-        right = currentNode
-        break;
-      } else if (currentNode.left.rank < currentNode.rank) {
-        const tmp = currentNode.left
-        currentNode.left = tmp.right
-        tmp.right = currentNode
-        currentNode = tmp
+      if (rightTail != null) {
+        rightTail.left = currentNode
+        currentNode.parent = rightTail
       } else {
         right = currentNode
-        currentNode = currentNode.left
       }
+      rightTail = currentNode
+      currentNode = currentNode.left
+      rightTail.left = null
     }
   }
-
   return [left, right]
 }
 
-function merge<V>(left: Nullable<Node<V>>, right: Nullable<Node<V>>): Nullable<Node<V>> {
-  let root: Nullable<Node<V>> = null
-  let currentNode: Nullable<Node<V>> = null
+function merge<V> (left: Nullable<Node<V>>, right: Nullable<Node<V>>): Nullable<Node<V>> {
+  if (left == null) return right
+  if (right == null) return left
 
-  while (left !== null && right !== null) {
-    if (left.rank < right.rank) {
-      if (root === null) {
-        root = left
-      } else {
-        currentNode!.left = left
-      }
-      currentNode = left
-      left = left.right
-      currentNode.right = null
-    } else {
-      if (root === null) {
-        root = right
-      } else {
-        currentNode!.right = right
-      }
-      currentNode = right
-      right = right.left
-      currentNode.left = null
-    }
+  let currentNode: Nullable<Node<V>> = left
+  while (currentNode.right !== null) {
+    currentNode = currentNode.right
   }
 
-  if (left !== null) {
-    if (root === null) {
-      root = left
-    } else {
-      currentNode!.right = left
-    }
-  }
+  currentNode.right = right
+  right.parent = currentNode
 
-  if (right !== null) {
-    if (root === null) {
-      root = right
-    } else {
-      currentNode!.left = right
-    }
-  }
-
-  return root
+  return left
 }
