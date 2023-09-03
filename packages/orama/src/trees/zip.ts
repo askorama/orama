7a3a336d007e66c906cdfde89eb125983cd5480c
Michele Riva <ciao@micheleriva.it>
2023-09-03 10:43:25 -0700

    feat(orama): adds zip tree (#430)

---
+++ packages/orama/src/trees/zip.ts
@@ -0,0 +1,247 @@
+import { Nullable } from '../types.js'
+
+interface Node<V = unknown> {
+  left: Nullable<Node<V>>
+  right: Nullable<Node<V>>
+  parent: Nullable<Node<V>>
+  key: number
+  value: V
+  rank: number
+}
+
+function randomRank(): number {
+  const r = Math.random()
+  return Math.floor(Math.log(1 - r) / Math.log(1 - 0.5))
+}
+
+export function create<V> (key: number, value: V): Node<V> {
+  return {
+    value,
+    key,
+    rank: randomRank(),
+    left: null,
+    right: null,
+    parent: null
+  }
+}
+
+export function insert<V> (root: Node<V>, key: number, value: V): void {
+  const newNode = create(key, value)
+  if (!root) {
+    return
+  }
+
+  let currentNode: Nullable<Node<V>> = root
+  let parent: Nullable<Node<V>> = null
+
+  while (currentNode != null) {
+    parent = currentNode
+    if (key < currentNode.key) {
+      currentNode = currentNode.left
+    } else if (key > currentNode.key) {
+      currentNode = currentNode.right
+    } else {
+      currentNode.value = value
+      return
+    }
+  }
+
+  if ((parent != null) && key < parent.key) {
+    parent.left = newNode
+  } else {
+    if (parent != null) {
+      parent.right = newNode
+    }
+  }
+  newNode.parent = parent
+}
+
+export function remove<V> (root: Nullable<Node<V>>, key: number): Nullable<Node<V>> {
+  if (root === null) {
+    return null
+  }
+
+  const [left, mid] = split(root, key)
+  const [toDelete, right] = split(mid, key + 1)
+  const newRoot = merge(left, right)
+
+  return newRoot
+}
+
+export function find<V> (root: Nullable<Node<V>>, key: number): Nullable<V> {
+  let currentNode: Nullable<Node<V>> = root
+
+  while (currentNode !== null) {
+    if (currentNode.key === key) {
+      return currentNode.value
+    }
+    currentNode = (currentNode.key < key) ? currentNode.right : currentNode.left
+  }
+
+  return null
+}
+
+export function contains<V> (root: Nullable<Node<V>>, key: number): boolean {
+  return !!find(root, key)
+}
+
+export function rangeSearch<V> (root: Nullable<Node<V>>, min: number, max: number): V[] {
+  const results: V[] = []
+  const stack: Array<Nullable<Node<V>>> = []
+  let currentNode = root
+
+  while (currentNode !== null || stack.length > 0) {
+    while (currentNode !== null) {
+      stack.push(currentNode)
+      currentNode = currentNode.left
+    }
+
+    currentNode = stack.pop()!
+    if (currentNode.key >= min && currentNode.key <= max) {
+      results.push(currentNode.value)
+    }
+
+    currentNode = currentNode.right
+  }
+
+  return results
+}
+
+export function greaterThan<V> (root: Nullable<Node<V>>, key: number): V[] {
+  const results: V[] = []
+  const stack: Array<Nullable<Node<V>>> = []
+  let currentNode = root
+
+  while (currentNode !== null || stack.length > 0) {
+    while (currentNode !== null) {
+      stack.push(currentNode)
+      currentNode = currentNode.left
+    }
+
+    currentNode = stack.pop()!
+    if (currentNode.key > key) {
+      results.push(currentNode.value)
+    }
+
+    currentNode = currentNode.right
+  }
+
+  return results
+}
+
+export function lessThan<V> (root: Nullable<Node<V>>, key: number): V[] {
+  const results: V[] = []
+  const stack: Array<Nullable<Node<V>>> = []
+  let currentNode = root
+
+  while (currentNode !== null || stack.length > 0) {
+    while (currentNode !== null) {
+      stack.push(currentNode)
+      currentNode = currentNode.left
+    }
+
+    currentNode = stack.pop()!
+    if (currentNode.key < key) {
+      results.push(currentNode.value)
+    }
+
+    currentNode = currentNode.right
+  }
+
+  return results
+}
+
+export function getSize<V> (root: Nullable<Node<V>>): number {
+  const stack: Array<Nullable<Node<V>>> = []
+  let count = 0
+  let currentNode = root
+
+  while (currentNode !== null || stack.length > 0) {
+    while (currentNode !== null) {
+      stack.push(currentNode)
+      currentNode = currentNode.left
+    }
+
+    currentNode = stack.pop()!
+    count++
+    currentNode = currentNode.right
+  }
+
+  return count
+}
+
+function split<V> (root: Nullable<Node<V>>, key: number): [Nullable<Node<V>>, Nullable<Node<V>>] {
+  let left: Nullable<Node<V>> = null
+  let leftTail: Nullable<Node<V>> = null
+  let right: Nullable<Node<V>> = null
+  let rightTail: Nullable<Node<V>> = null
+  let currentNode = root
+
+  while (currentNode !== null) {
+    if (currentNode.key < key) {
+      if (leftTail != null) {
+        leftTail.right = currentNode
+        currentNode.parent = leftTail
+      } else {
+        left = currentNode
+      }
+      leftTail = currentNode
+      currentNode = currentNode.right
+      leftTail.right = null
+    } else {
+      if (rightTail != null) {
+        rightTail.left = currentNode
+        currentNode.parent = rightTail
+      } else {
+        right = currentNode
+      }
+      rightTail = currentNode
+      currentNode = currentNode.left
+      rightTail.left = null
+    }
+  }
+  return [left, right]
+}
+
+function merge<V> (left: Nullable<Node<V>>, right: Nullable<Node<V>>): Nullable<Node<V>> {
+  if (left == null) return right
+  if (right == null) return left
+
+  let currentNode: Nullable<Node<V>> = left
+  while (currentNode.right !== null) {
+    currentNode = currentNode.right
+  }
+
+  currentNode.right = right
+  right.parent = currentNode
+
+  return left
+}
+
+export function removeDocument<V> (root: Node<V[]>, id: V, key: number): void {
+  const node = getNodeByKey(root, key)
+
+  if (node == null) return
+
+  if (node.value.length === 1 && node.value[0] === id) {
+    remove(root, key)
+  } else {
+    const index = node.value.indexOf(id)
+    if (index !== -1) {
+      node.value.splice(index, 1)
+    }
+  }
+}
+
+function getNodeByKey<V> (root: Nullable<Node<V>>, key: number): Nullable<Node<V>> {
+  let currentNode: Nullable<Node<V>> = root
+
+  while (currentNode !== null) {
+    if (currentNode.key === key) {
+      return currentNode
+    }
+    currentNode = (currentNode.key < key) ? currentNode.right : currentNode.left
+  }
+
+  return null
+}
---
+++ packages/orama/tests/tree.zip.test.ts
@@ -0,0 +1,119 @@
+import t from 'tap'
+import {
+  create,
+  insert,
+  find,
+  getSize,
+  remove,
+  contains,
+  greaterThan,
+  lessThan,
+  rangeSearch,
+} from '../src/trees/zip.js'
+
+t.test('ZIP Tree', t => {
+  t.plan(7)
+
+  t.test('create', t => {
+    t.plan(3)
+
+    const tree = create(1, 'foo')
+    t.equal(getSize(tree), 1)
+    t.equal(find(tree, 1), 'foo')
+    t.equal(find(tree, 4), null)
+  })
+
+  t.test('insert', t => {
+    t.plan(1)
+
+    const tree = create(1, 'foo')
+
+    insert(tree, 2, 'bar')
+    insert(tree, 10, 'baz')
+    insert(tree, 25, 'qux')
+    insert(tree, 5, 'quux')
+    insert(tree, 20, 'quuz')
+    insert(tree, 12, 'corge')
+
+    t.equal(getSize(tree), 7)
+  })
+
+  t.test('find', t => {
+    t.plan(2)
+
+    const tree = create(1, [1, 2, 3])
+
+    insert(tree, 2, [4, 5, 6])
+    insert(tree, 10, [7, 8, 9])
+    insert(tree, 25, [10, 11, 12])
+    insert(tree, 5, [13, 14, 15])
+    insert(tree, 20, [16, 17, 18])
+    insert(tree, 12, [19, 20, 21])
+
+    t.same(contains(tree, 20), true)
+    t.same(find(tree, 20), [16, 17, 18])
+  })
+
+  t.test('remove', t => {
+    t.plan(2)
+
+    const tree = create(1, 'foo')
+
+    insert(tree, 2, 'bar')
+    insert(tree, 10, 'baz')
+    insert(tree, 25, 'qux')
+    insert(tree, 5, 'quux')
+    insert(tree, 20, 'quuz')
+    insert(tree, 12, 'corge')
+
+    remove(tree, 20)
+
+    t.equal(getSize(tree), 6)
+    t.equal(contains(tree, 20), false)
+  })
+
+  t.test('rangeSearch', t => {
+    t.plan(1)
+
+    const tree = create(1, 'foo')
+
+    insert(tree, 2, 'bar')
+    insert(tree, 10, 'baz')
+    insert(tree, 25, 'qux')
+    insert(tree, 5, 'quux')
+    insert(tree, 20, 'quuz')
+    insert(tree, 12, 'corge')
+
+    t.same(rangeSearch(tree, 5, 20), ['quux', 'baz', 'corge', 'quuz'])
+  })
+
+  t.test('greaterThan', t => {
+    t.plan(1)
+
+    const tree = create(1, 'foo')
+
+    insert(tree, 2, 'bar')
+    insert(tree, 10, 'baz')
+    insert(tree, 25, 'qux')
+    insert(tree, 5, 'quux')
+    insert(tree, 20, 'quuz')
+    insert(tree, 12, 'corge')
+
+    t.same(greaterThan(tree, 10), ['corge', 'quuz', 'qux'])
+  })
+
+  t.test('lessThan', t => {
+    t.plan(1)
+
+    const tree = create(1, 'foo')
+
+    insert(tree, 2, 'bar')
+    insert(tree, 10, 'baz')
+    insert(tree, 25, 'qux')
+    insert(tree, 5, 'quux')
+    insert(tree, 20, 'quuz')
+    insert(tree, 12, 'corge')
+
+    t.same(lessThan(tree, 10), ['foo', 'bar', 'quux'])
+  })
+})
