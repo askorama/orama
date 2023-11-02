b927dd32e55fa81c7a5b0ee2106c6b545e1f839d
Tommaso Allevi <tomallevi@gmail.com>
2023-11-02 12:19:05 +0100

    Fix #538

---
+++ packages/orama/tests/type/issue_538.test-d.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// https://github.com/oramasearch/orama/issues/538
+import { expectAssignable } from 'tsd'
+import { create, search } from '../../src/index.ts'
+
+const movieSchema = {
+  title: 'string',
+} as const
+const db = await create({ schema: movieSchema })
+
+interface Movie {
+  title: string,
+  year: number,
+}
+
+const r = await search<typeof db, Movie>(db, { term: '' })
+expectAssignable<string>(r.hits[0].document.title)
+expectAssignable<number>(r.hits[0].document.year)
