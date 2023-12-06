02047c229dc796ce2442ddb24adb8fb5beb21c26
Michele Riva <ciao@micheleriva.it>
2023-12-06 19:54:38 +0100

    removes classname

--- packages/plugin-docusaurus-dev/src/client/theme/SearchBar/index.js
+++ packages/plugin-docusaurus-dev/src/client/theme/SearchBar/index.js
@@ -89,8 +89,16 @@ export function OramaSearch () {
         className='DocSearch DocSearch-Button'
         onClick={() => $.setShow(true)}
       >
-        <span class="DocSearch-Button-Container"><svg width="20" height="20" class="DocSearch-Search-Icon" viewBox="0 0 20 20"><path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"></path></svg><span class="DocSearch-Button-Placeholder">Search</span></span>
-        <span class="DocSearch-Button-Keys"><kbd class="DocSearch-Button-Key">⌘</kbd><kbd class="DocSearch-Button-Key">K</kbd></span>
+        <span className="DocSearch-Button-Container">
+          <svg width="20" height="20" class="DocSearch-Search-Icon" viewBox="0 0 20 20">
+            <path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+            </path>
+            </svg>
+          <span className="DocSearch-Button-Placeholder">
+            Search
+          </span>
+        </span>
+        <span className="DocSearch-Button-Keys"><kbd className="DocSearch-Button-Key">⌘</kbd><kbd className="DocSearch-Button-Key">K</kbd></span>
       </button>
 
       {oramaInstance && (
