d093ba1d4576da7639a3041be2a28d8fcb2aca50
Michele Riva <ciao@micheleriva.it>
2023-12-12 16:03:25 +0100

    feat: moves sandbox into new sandboxes workspace

--- packages/plugin-docusaurus-v3/scripts/postbuild.sh
+++ packages/plugin-docusaurus-v3/scripts/postbuild.sh
@@ -9,7 +9,3 @@ tsc --module CommonJS --outDir dist/server/cjs/ src/server/types.ts
 mv dist/server/commonjs.js dist/server/commonjs.cjs
 mv dist/server/commonjs.js.map dist/server/commonjs.cjs.map
 mv dist/server/cjs/types.js dist/server/types.cjs && rm -r dist/server/cjs
-
-cp -a src/translationMessages dist/translationMessages
-cp src/client/theme/SearchBar/*.css dist/client/theme/SearchBar
-cp src/client/theme/SearchBarFooter/*.css dist/client/theme/SearchBarFooter
