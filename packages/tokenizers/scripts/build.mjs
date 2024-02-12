a156db25aec4873f679668c3ad3f3afc4b413fd1
Michele Riva <ciao@micheleriva.it>
2024-02-13 00:06:50 +0100

    skip build on CI

--- packages/tokenizers/scripts/build.mjs
+++ packages/tokenizers/scripts/build.mjs
@@ -2,6 +2,11 @@ import path from 'node:path'
 import fs from 'node:fs'
 import childProcess from 'node:child_process'
 
+if (process.env.GITHUB_ACTIONS) {
+  console.log('Skipping build in CI')
+  process.exit(0)
+}
+
 const outdirBaseURL = new URL('../dist', import.meta.url).pathname
 const tokenizersBaseURL = new URL('../src', import.meta.url).pathname
 
