63721db7bcaa486e7aebc35c50c6b4e3a1152030
Michele Riva <ciao@micheleriva.it>
2024-02-13 00:22:09 +0100

    Adds Mandarin tokenizer (#628)

---
+++ packages/tokenizers/scripts/build.mjs
@@ -0,0 +1,27 @@
+import path from 'node:path'
+import fs from 'node:fs'
+import childProcess from 'node:child_process'
+
+if (process.env.GITHUB_ACTIONS) {
+  console.log('Skipping build in CI')
+  process.exit(0)
+}
+
+const outdirBaseURL = new URL('../dist', import.meta.url).pathname
+const tokenizersBaseURL = new URL('../src', import.meta.url).pathname
+
+const mandarinTokenizerPath = path.join(tokenizersBaseURL, 'tokenizer-mandarin')
+const mandarinTokenizerWasmPath = path.join(mandarinTokenizerPath, 'pkg')
+const mandarinTokenizerDistPath = path.join(tokenizersBaseURL, '../dist/tokenizer-mandarin')
+
+if (fs.existsSync(outdirBaseURL)) {
+  fs.rmdirSync(outdirBaseURL, { recursive: true })
+}
+
+fs.mkdirSync(outdirBaseURL)
+
+childProcess.execSync(`cd ${mandarinTokenizerPath} && wasm-pack build --target web`)
+
+fs.cpSync(mandarinTokenizerWasmPath, mandarinTokenizerDistPath, {
+  recursive: true
+})
---
+++ packages/tokenizers/src/tokenizer-mandarin/src/lib.rs
@@ -0,0 +1,11 @@
+use jieba_rs::Jieba;
+use serde_json::to_string;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn tokenize(sentence: &str, hmm: bool) -> JsValue {
+    let jieba = Jieba::new();
+    let words = jieba.cut(sentence, hmm);
+    let serialized_words = to_string(&words).unwrap_or_default();
+    JsValue::from_str(&serialized_words)
+}
