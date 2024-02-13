0c1684b4efcc8a4558258ae9be85e988a1b881b4
Tommaso Allevi <tomallevi@gmail.com>
2024-02-13 14:25:44 +0100

    Remove test file

--- packages/tokenizers/test.ts
+++
@@ -1,8 +0,0 @@
-import {createTokenizer} from './dist/tokenizer-mandarin/tokenizer.mjs'
-
-console.log(createTokenizer)
-
-const t = await createTokenizer({ language: 'mandarin' })
-
-const a = await t.tokenize('你好吗？')
-console.log(a)
