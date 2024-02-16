1cc68acbfd0325c590a1f441d774dc3b23d8f2cb
Michele Riva <ciao@micheleriva.it>
2024-02-16 16:48:51 +0100

    fix: fixes tokenizers dist (#637)

--- packages/tokenizers/scripts/build.mjs
+++ packages/tokenizers/scripts/build.mjs
@@ -7,12 +7,12 @@ if (process.env.GITHUB_ACTIONS) {
   process.exit(0)
 }
 
-const outdirBaseURL = new URL('../dist', import.meta.url).pathname
+const outdirBaseURL = new URL('../build', import.meta.url).pathname
 const tokenizersBaseURL = new URL('../src', import.meta.url).pathname
 
 const mandarinTokenizerPath = path.join(tokenizersBaseURL, 'tokenizer-mandarin')
 const mandarinTokenizerWasmPath = path.join(mandarinTokenizerPath, 'pkg')
-const mandarinTokenizerDistPath = path.join(tokenizersBaseURL, '../dist/tokenizer-mandarin')
+const mandarinTokenizerDistPath = path.join(tokenizersBaseURL, '../build/tokenizer-mandarin')
 const mandarinTokenizerWrapperPath = path.join(tokenizersBaseURL, 'tokenizer-mandarin/src/tokenizer.ts')
 const mandarinTokenizerWrapperDistPath = path.join(mandarinTokenizerDistPath, 'tokenizer.ts')
 
@@ -32,9 +32,11 @@ fs.cpSync(mandarinTokenizerWasmPath, mandarinTokenizerDistPath, {
   recursive: true
 })
 
-const r = fs.readFileSync('./dist/tokenizer-mandarin/tokenizer_mandarin_bg.wasm')
+fs.rmSync(path.join(mandarinTokenizerDistPath, '.gitignore'))
+
+const r = fs.readFileSync('./build/tokenizer-mandarin/tokenizer_mandarin_bg.wasm')
 const b = new Uint8Array(r)
 const rr = `export const wasm = new Uint8Array([${b.join(',')}]);`
-fs.writeFileSync('./dist/tokenizer-mandarin/tokenizer_mandarin_bg_wasm_arr.js', rr)
+fs.writeFileSync('./build/tokenizer-mandarin/tokenizer_mandarin_bg_wasm_arr.js', rr)
 
 childProcess.execSync(`cd ${mandarinTokenizerDistPath} && npx tsup --format cjs,esm,iife --outDir . tokenizer.ts`)
