4fa2951f84913681831ca9febe0efdc5fb04529e
Michele Riva <ciao@micheleriva.it>
2024-06-30 20:35:44 +0200

    improves docs

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -2,24 +2,12 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import react from "@astrojs/react";
 import starlightImageZoom from "starlight-image-zoom";
-import orama from "./plugins";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://docs.oramasearch.com",
+  site: "https://docs.askorama.ai",
   integrations: [
-    orama({
-      // We can generate more than one DB, with different configurations
-      docs: {
-        // Required. Only pages matching this path regex will be indexed
-        pathMatcher: /(cloud|open-source)\/*/,
-
-        // // Optional. ['body'] by default. Use it to constraint what is used to
-        // // index a page.
-        contentSelectors: [".sl-markdown-content"],
-      },
-    }),
     starlight({
       pagefind: false,
       plugins: [starlightImageZoom()],
