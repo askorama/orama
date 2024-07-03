ef2dbf703b65b5c9bc9f6546255a66056d2753b6
Michele Riva <ciao@micheleriva.it>
2024-07-03 14:56:37 +0200

    chore: temporary fix build for docusaurus v2

--- packages/plugin-docusaurus/scripts/postbuild.sh
+++ packages/plugin-docusaurus/scripts/postbuild.sh
@@ -2,9 +2,9 @@
 
 set -x -e
 
-tsc -p . --emitDeclarationOnly
-tsc -p tsconfig.cjs.json --emitDeclarationOnly
-tsc --module CommonJS --outDir dist/server/cjs/ src/server/types.ts
+# tsc -p . --emitDeclarationOnly
+# tsc -p tsconfig.cjs.json --emitDeclarationOnly
+# tsc --module CommonJS --outDir dist/server/cjs/ src/server/types.ts
 
 mv dist/server/commonjs.js dist/server/commonjs.cjs
 mv dist/server/commonjs.js.map dist/server/commonjs.cjs.map
