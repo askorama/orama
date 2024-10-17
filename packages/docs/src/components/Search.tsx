22d43e74527168079e129765f320e7bf05a18731
Fausto Quaggia <fausto.quaggia@gmail.com>
2024-10-17 16:45:18 +0200

    docs: updates search

--- packages/docs/src/components/Search.tsx
+++ packages/docs/src/components/Search.tsx
@@ -131,6 +131,9 @@ export function Search() {
         suggestions={suggestions}
         searchPlaceholder={`Search ${currentCategory === 'Open Source' ? 'Open Source' : 'Orama Cloud'}`}
         chatPlaceholder='Ask me anything about Orama'
+        highlight={{
+          HTMLTag: 'b'
+        }}
       />
 
       <OramaSearchButton
